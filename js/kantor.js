c490c2134f20ddfdab88c0ca152be6c983559505
Igor Kozlowski <igor3648@gmail.com>
2023-02-05 16:43:53 +0100

    calculations value changed from string to numer

--- js/kantor.js
+++ js/kantor.js
@@ -6,84 +6,79 @@ const resultElement = document.querySelector(".js-result");
 
 const pln = {
   short: "PLN",
-  value: "1",
+  value: 1,
   name: "Polski złoty",
 };
 
 const gbp = {
   short: "GBP",
-  value: "5,43913",
+  value: 5.43913,
   name: "Polski złoty",
 };
 
 const usd = {
   short: "USD",
-  value: "4,68485",
+  value: 4.68485,
   name: "Dolar amerykański",
 };
 
 const czk = {
   short: "CZK",
-  value: "0,19224",
+  value: 0.19224,
   name: "Korona czeska",
 };
 
 const eur = {
   short: "EUR",
-  value: "4,71790",
+  value: 4.71790,
   name: "Euro",
 };
-
-const plnToGbp = 0.18385;
-const plnToUsd = 0.21345;
-const plnToCzk = 5.20183;
-const plnToEur = 0.21195;
 
 const calculateResult = (inputCurrency, outputCurrency, amount) => {
   switch (inputCurrency) {
     case pln.short:
       switch (outputCurrency) {
         case pln.short: return +amount;
-        case eur.short: return amount * plnToEur;
-        case czk.short: return amount * plnToCzk;
-        case usd.short: return amount * plnToUsd;
-        case gbp.short: return amount * plnToGbp;
+        case eur.short: return amount / eur.value;
+        case czk.short: return amount / czk.value;
+        case usd.short: return amount / usd.value;
+        case gbp.short: return amount / gbp.value;
+      }
+      break;
+      case gbp.short:
+      switch (outputCurrency) {
+        case gbp.short: return +amount;
+        case pln.short: return amount * gbp.value;
+        case eur.short: return amount * gbp.value / eur.value;
+        case czk.short: return amount * gbp.value / czk.value;
+        case usd.short: return amount * gbp.value / usd.value;
       }
       break;
     case usd.short:
       switch (outputCurrency) {
         case usd.short: return +amount;
-        case pln.short: return amount / plnToUsd;
-        case eur.short: return amount * plnToEur / plnToUsd;
-        case czk.short: return amount * plnToCzk / plnToUsd;
-        case gbp.short: return amount * plnToGbp / plnToUsd;
-      }
-      break;
-    case gbp.short:
-      switch (outputCurrency) {
-        case gbp.short: return +amount;
-        case pln.short: return amount / plnToGbp;
-        case eur.short: return amount * plnToEur / plnToGbp;
-        case czk.short: return amount * plnToCzk / plnToGbp;
-        case usd.short: return amount * plnToUsd / plnToGbp;
+        case pln.short: return amount * usd.value;
+        case eur.short: return amount * usd.value / eur.value;
+        case czk.short: return amount * usd.value / czk.value;
+        case gbp.short: return amount * usd.value / gbp.value;
       }
       break;
     case czk.short:
       switch (outputCurrency) {
         case czk.short: return +amount;
-        case pln.short: return amount / plnToCzk;
-        case eur.short: return amount * plnToEur / plnToCzk;
-        case gbp.short: return amount * plnToGbp / plnToCzk;
-        case usd.short: return amount * plnToUsd / plnToCzk;
+        case pln.short: return amount * czk.value;
+        case eur.short: return amount * czk.value / eur.value;
+        case gbp.short: return amount * czk.value / gbp.value;
+        case usd.short: return amount * czk.value / usd.value;
       }
       break;
     case eur.short:
       switch (outputCurrency) {
         case eur.short: return +amount;
-        case pln.short: return amount / plnToEur;
-        case czk.short: return amount * plnToCzk / plnToEur;
-        case gbp.short: return amount * plnToGbp / plnToEur;
-        case usd.short: return amount * plnToEur / plnToEur;
+        case pln.short: return amount * eur.value;
+        case czk.short: return amount * eur.value / czk.value;
+        case gbp.short: return amount * eur.value / gbp.value;
+        case usd.short: return amount * eur.value / usd.value;
       }
       break;
   };
@@ -92,12 +87,12 @@ const calculateResult = (inputCurrency, outputCurrency, amount) => {
 const onFormSubmit = (event) => {
   event.preventDefault();
 
-  const inputCurrency = inputCurrencyElement.short;
-  const outputCurrency = outputCurrencyElement.short;
-  const amount = amountElement.short;
+  const inputCurrency = inputCurrencyElement.value;
+  const outputCurrency = outputCurrencyElement.value;
+  const amount = amountElement.value;
 
   const result = calculateResult(inputCurrency, outputCurrency, amount);
 
-  resultElement.innerText = `${result} ${outputCurrency}`;
+  resultElement.innerText = `${result.toFixed(2)} ${outputCurrency}`;
 };
 formElement.addEventListener("submit", onFormSubmit)
